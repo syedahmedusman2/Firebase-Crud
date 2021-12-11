0874bf1b7cf4a15b463631ca80a924e3955b0551
syedahmedusman2 <66121118+syedahmedusman2@users.noreply.github.com>
2021-12-12 00:10:51 +0500

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -16,9 +16,10 @@ class _HomepageState extends State<Homepage> {
     FirebaseFirestore.instance.collection('tasks').add({
       'task': taskController.text,
     });
-    final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('tasks')
-        .snapshots();
   }
+
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('tasks').snapshots();
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +53,75 @@ class _HomepageState extends State<Homepage> {
               )
             ],
           ),
-        ))
+        )),
+        //   StreamBuilder<QuerySnapshot>(
+        //   stream: _usersStream,
+        //   builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        //     if (snapshot.hasError) {
+        //       return Text('Something went wrong');
+        //     }
+
+        //     if (snapshot.connectionState == ConnectionState.waiting) {
+        //       return Text("Loading");
+        //     }
+
+        //     return Container(
+        //       child: ListView(
+        //         children: snapshot.data!.docs.map((DocumentSnapshot document) {
+        //           Map<String, dynamic> data =
+        //               document.data()! as Map<String, dynamic>;
+        //           return Container(
+        //             decoration: BoxDecoration(
+        //               //                    <-- BoxDecoration
+        //               border: Border(bottom: BorderSide()),
+        //             ),
+        //             child: ListTile(
+        //               title: Column(
+        //                 crossAxisAlignment: CrossAxisAlignment.start,
+
+        //                 children: [
+        //                   SizedBox(height: 5,),
+        //                   Text(
+        //                     'Date: ${data['Date']}',
+        //                     style: TextStyle(
+        //                         fontSize: 16.0, fontWeight: FontWeight.bold),
+        //                   ),
+        //                   SizedBox(height: 5,),
+        //                   // subtitle:
+        //                   Text(
+        //                     'Morning Rate: ${data['Morning Rate']}',
+        //                     style: TextStyle(fontSize: 16.0),
+        //                   ),
+        //                   SizedBox(height: 5,),
+        //                   Text(
+        //                     'Evening Rate: ${data['Evening Rate']}',
+        //                     style: TextStyle(fontSize: 16.0),
+        //                   ),
+
+        //                 ],
+        //               ),
+        //               //
+        //               trailing: IconButton(
+        //                   onPressed: () {
+        //                     document.reference.delete();
+        //                   },
+        //                   icon: Icon(Icons.delete)),
+
+        //               // trailing: Column(
+        //               //   children:[
+        //               //     SizedBox(height: 3.0,),
+
+        //               //   ],
+
+        //               // ),
+        //               // subtitle: Text(data['Morning Rate']),
+        //             ),
+        //           );
+        //         }).toList(),
+        //       ),
+        //     );
+        //   },
+        // ),
       ],
     );
   }
