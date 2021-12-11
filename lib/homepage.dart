af0741c94c6e69e0890cc1c3a7e0a6934a305231
syedahmedusman2 <66121118+syedahmedusman2@users.noreply.github.com>
2021-12-12 00:18:41 +0500

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -62,74 +62,74 @@ class _HomepageState extends State<Homepage> {
                 ],
               ),
             )),
-            //   StreamBuilder<QuerySnapshot>(
-            //   stream: _usersStream,
-            //   builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            //     if (snapshot.hasError) {
-            //       return Text('Something went wrong');
-            //     }
-    
-            //     if (snapshot.connectionState == ConnectionState.waiting) {
-            //       return Text("Loading");
-            //     }
-    
-            //     return Container(
-            //       child: ListView(
-            //         children: snapshot.data!.docs.map((DocumentSnapshot document) {
-            //           Map<String, dynamic> data =
-            //               document.data()! as Map<String, dynamic>;
-            //           return Container(
-            //             decoration: BoxDecoration(
-            //               //                    <-- BoxDecoration
-            //               border: Border(bottom: BorderSide()),
-            //             ),
-            //             child: ListTile(
-            //               title: Column(
-            //                 crossAxisAlignment: CrossAxisAlignment.start,
-    
-            //                 children: [
-            //                   SizedBox(height: 5,),
-            //                   Text(
-            //                     'Date: ${data['Date']}',
-            //                     style: TextStyle(
-            //                         fontSize: 16.0, fontWeight: FontWeight.bold),
-            //                   ),
-            //                   SizedBox(height: 5,),
-            //                   // subtitle:
-            //                   Text(
-            //                     'Morning Rate: ${data['Morning Rate']}',
-            //                     style: TextStyle(fontSize: 16.0),
-            //                   ),
-            //                   SizedBox(height: 5,),
-            //                   Text(
-            //                     'Evening Rate: ${data['Evening Rate']}',
-            //                     style: TextStyle(fontSize: 16.0),
-            //                   ),
-    
-            //                 ],
-            //               ),
-            //               //
-            //               trailing: IconButton(
-            //                   onPressed: () {
-            //                     document.reference.delete();
-            //                   },
-            //                   icon: Icon(Icons.delete)),
-    
-            //               // trailing: Column(
-            //               //   children:[
-            //               //     SizedBox(height: 3.0,),
-    
-            //               //   ],
-    
-            //               // ),
-            //               // subtitle: Text(data['Morning Rate']),
-            //             ),
-            //           );
-            //         }).toList(),
-            //       ),
-            //     );
-            //   },
-            // ),
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                stream: _usersStream,
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Something went wrong');
+                  }
+                  
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  
+                  return Container(
+                    child: ListView(
+                      children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                        Map<String, dynamic> data =
+                            document.data()! as Map<String, dynamic>;
+                        return Container(
+                          decoration: BoxDecoration(
+                            //                    <-- BoxDecoration
+                            border: Border(bottom: BorderSide()),
+                          ),
+                          child: ListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                  
+                              children: [
+                                SizedBox(height: 5,),
+                                // Text(
+                                //   'Date: ${data['Date']}',
+                                //   style: TextStyle(
+                                //       fontSize: 16.0, fontWeight: FontWeight.bold),
+                                // ),
+                                SizedBox(height: 5,),
+                                // subtitle:
+                                
+                                Text(
+                                  'Task: ${data['task']}',
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                  
+                              ],
+                            ),
+                            //
+                            trailing: IconButton(
+                                onPressed: () {
+                                  document.reference.delete();
+                                },
+                                icon: Icon(Icons.delete)),
+                  
+                            // trailing: Column(
+                            //   children:[
+                            //     SizedBox(height: 3.0,),
+                  
+                            //   ],
+                  
+                            // ),
+                            // subtitle: Text(data['Morning Rate']),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  );
+                },
+                          ),
+              ),
           ],
         ),
       ),
