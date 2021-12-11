479432f6491272cb3f5c1f0345e5c35b6d1b9ee6
syedahmedusman2 <66121118+syedahmedusman2@users.noreply.github.com>
2021-12-12 00:14:20 +0500

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -23,106 +23,111 @@ class _HomepageState extends State<Homepage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            child: Card(
-          child: Column(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.7,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 1.0,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+      ),
+      body: Column(
+        children: [
+          Container(
+              child: Card(
+            child: Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1.0,
+                    ),
+                  ),
+                  child: TextField(
+                    controller: taskController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Task',
+                    ),
                   ),
                 ),
-                child: TextField(
-                  controller: taskController,
-                  decoration: const InputDecoration(
-                    hintText: 'Enter Task',
-                  ),
-                ),
-              ),
-              OutlineButton(
-                onPressed: () {
-                  addData();
-                },
-                child: const Text('Add Task'),
-              )
-            ],
-          ),
-        )),
-        //   StreamBuilder<QuerySnapshot>(
-        //   stream: _usersStream,
-        //   builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        //     if (snapshot.hasError) {
-        //       return Text('Something went wrong');
-        //     }
-
-        //     if (snapshot.connectionState == ConnectionState.waiting) {
-        //       return Text("Loading");
-        //     }
-
-        //     return Container(
-        //       child: ListView(
-        //         children: snapshot.data!.docs.map((DocumentSnapshot document) {
-        //           Map<String, dynamic> data =
-        //               document.data()! as Map<String, dynamic>;
-        //           return Container(
-        //             decoration: BoxDecoration(
-        //               //                    <-- BoxDecoration
-        //               border: Border(bottom: BorderSide()),
-        //             ),
-        //             child: ListTile(
-        //               title: Column(
-        //                 crossAxisAlignment: CrossAxisAlignment.start,
-
-        //                 children: [
-        //                   SizedBox(height: 5,),
-        //                   Text(
-        //                     'Date: ${data['Date']}',
-        //                     style: TextStyle(
-        //                         fontSize: 16.0, fontWeight: FontWeight.bold),
-        //                   ),
-        //                   SizedBox(height: 5,),
-        //                   // subtitle:
-        //                   Text(
-        //                     'Morning Rate: ${data['Morning Rate']}',
-        //                     style: TextStyle(fontSize: 16.0),
-        //                   ),
-        //                   SizedBox(height: 5,),
-        //                   Text(
-        //                     'Evening Rate: ${data['Evening Rate']}',
-        //                     style: TextStyle(fontSize: 16.0),
-        //                   ),
-
-        //                 ],
-        //               ),
-        //               //
-        //               trailing: IconButton(
-        //                   onPressed: () {
-        //                     document.reference.delete();
-        //                   },
-        //                   icon: Icon(Icons.delete)),
-
-        //               // trailing: Column(
-        //               //   children:[
-        //               //     SizedBox(height: 3.0,),
-
-        //               //   ],
-
-        //               // ),
-        //               // subtitle: Text(data['Morning Rate']),
-        //             ),
-        //           );
-        //         }).toList(),
-        //       ),
-        //     );
-        //   },
-        // ),
-      ],
+                OutlineButton(
+                  onPressed: () {
+                    addData();
+                  },
+                  child: const Text('Add Task'),
+                )
+              ],
+            ),
+          )),
+          //   StreamBuilder<QuerySnapshot>(
+          //   stream: _usersStream,
+          //   builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          //     if (snapshot.hasError) {
+          //       return Text('Something went wrong');
+          //     }
+    
+          //     if (snapshot.connectionState == ConnectionState.waiting) {
+          //       return Text("Loading");
+          //     }
+    
+          //     return Container(
+          //       child: ListView(
+          //         children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          //           Map<String, dynamic> data =
+          //               document.data()! as Map<String, dynamic>;
+          //           return Container(
+          //             decoration: BoxDecoration(
+          //               //                    <-- BoxDecoration
+          //               border: Border(bottom: BorderSide()),
+          //             ),
+          //             child: ListTile(
+          //               title: Column(
+          //                 crossAxisAlignment: CrossAxisAlignment.start,
+    
+          //                 children: [
+          //                   SizedBox(height: 5,),
+          //                   Text(
+          //                     'Date: ${data['Date']}',
+          //                     style: TextStyle(
+          //                         fontSize: 16.0, fontWeight: FontWeight.bold),
+          //                   ),
+          //                   SizedBox(height: 5,),
+          //                   // subtitle:
+          //                   Text(
+          //                     'Morning Rate: ${data['Morning Rate']}',
+          //                     style: TextStyle(fontSize: 16.0),
+          //                   ),
+          //                   SizedBox(height: 5,),
+          //                   Text(
+          //                     'Evening Rate: ${data['Evening Rate']}',
+          //                     style: TextStyle(fontSize: 16.0),
+          //                   ),
+    
+          //                 ],
+          //               ),
+          //               //
+          //               trailing: IconButton(
+          //                   onPressed: () {
+          //                     document.reference.delete();
+          //                   },
+          //                   icon: Icon(Icons.delete)),
+    
+          //               // trailing: Column(
+          //               //   children:[
+          //               //     SizedBox(height: 3.0,),
+    
+          //               //   ],
+    
+          //               // ),
+          //               // subtitle: Text(data['Morning Rate']),
+          //             ),
+          //           );
+          //         }).toList(),
+          //       ),
+          //     );
+          //   },
+          // ),
+        ],
+      ),
     );
   }
 }
